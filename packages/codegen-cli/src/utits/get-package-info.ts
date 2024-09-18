ed4bc77371ccee77a378c575b88cb3b5550d3aee
leocoder <singhra66@gmail.com>
2024-09-19 00:43:03 +0530

    fix: relative path fix

--- packages/codegen-cli/src/utits/get-package-info.ts
+++ packages/codegen-cli/src/utits/get-package-info.ts
@@ -1,9 +1,16 @@
 import path from "path";
 import fs from "fs-extra"
+import { fileURLToPath } from 'url';
 import { type PackageJson } from "type-fest";
+import { logger } from "./logger";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootDir = path.resolve(__dirname, '..');
 
 export async function getPackageInfo() {
-    const packageJsonPath = path.join("package.json")
+    // logger.info("Script is running... with dir: ", path.join(rootDir, "package.json"));
+    const packageJsonPath = path.join(rootDir, "package.json")
     
     return fs.readJSONSync(packageJsonPath) as PackageJson
 }
